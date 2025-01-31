588c0d849ee3ad9d0b7e7432da65a1f0233b2f60
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-01-31 10:00:53 -0500

    Run in a loop for all entries of a list

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ import os
 
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Initialize the model
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api_key))
 
 async def main():
     agent = Agent(
         task=(
-            "What badge does stackoverflow award to users who meet with a stack exchange employee? Let's refer to this badge as x-badge"
-            "Find user user835611 on StackOverflow. You can accept all cookies. What reputation score do they have?. "
+            "Open file:///Users/franzih/code/operator/users.txt. "
+            "For every name in that list, find the user on StackOverflow. You can accept all cookies. What reputation score do they have?. "
             "Return a summary of topics they contribute to. "
-            "Has user835611 earned x-badge? "
         ),
         llm=llm,
     )
