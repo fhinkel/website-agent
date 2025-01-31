f78dd1cc3ec364838ba9ebea0f266667af3c611a
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-01-31 09:22:26 -0500

    Run with with Gemini 2.0

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 
-# website-agent
+# Demo for browser_use with Gemini
 
-Demo for using browser_use with Gemma open models.
-
-This example uses Gemma to control a Chrome browser. The code provided showcases how to:
+This example uses Gemini 2.0 to control a Chrome browser. The code provided showcases how to:
 
 * **Initialize a Chrome browser instance:**  Including configuring Chrome to use a specific profile.
 * **Define tasks for the agent:**  Provide natural language instructions to the agent to specify the actions it should perform in the browser.
 * **Utilize an LLM:**  Integrate with a large language model (in this case, OpenAI's GPT models) to interpret instructions and guide the agent's actions.
 * **Execute the agent:** Run the agent and observe the results of its browser interactions.
 
-This specific example explores retrieving information from websites like Reddit and Stack Overflow by providing natural language queries to the agent. It highlights Gemma's capability to understand complex instructions, navigate websites, extract data, and summarize findings.
+This specific example explores retrieving information from websites like Reddit and Stack Overflow by providing natural language queries to the agent. It highlights Gemini's capability to understand complex instructions, navigate websites, extract data, and summarize findings.
--- main.py
+++ main.py
@@ -1,44 +1,24 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from browser_use import Agent, Browser, BrowserConfig
 import asyncio
-from dotenv import load_dotenv
-load_dotenv()
 
+from pydantic import SecretStr
+import os
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+# Initialize the model
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api_key))
 
 async def main():
-
-    # Configure Chrome profile instead of incognito window
-    browser = Browser(
-        config=BrowserConfig(
-            chrome_instance_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-            # chrome_instance_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome --profile-directory=\"Profile 1\"'
-            extra_chromium_args=['--profile-directory=Default'],
-        )
-    )
-
     agent = Agent(
-        # task=("Go to google and search for MSFT stock."
-        # "Open the first result but ignore Market Watch and Yahoo"
-        # "Return a summary of why the stock dropped today"
-        # "got back to google and open the next result"
-        # "Return the title and a summary of why the stock dropped. Begin and end that summary with '------'"
-        # ),
-        # task=(
-        #     "Use the profile for Franziska Hinkelmann. "
-        #     "Got to reddit and find the username of Martin Omander. "
-        #     "Find his post history for the last 60 days. "
-        #     "Return his posts related to Google Cloud Platform (that includes products such as GKE, Kubernetes, Code Assist, Gemini, Vertix, Google Cloud, GCP, and others). "
-        #     "Summarize his expertise in one paragraph starting with a line break and wrap the summary in ********. "
-        #     "Find his top 3 comments with the most upvotes and list the title of the corresponding posts. "
-        # ),
         task=(
             "What badge does stackoverflow award to users who meet with a stack exchange employee? Let's refer to this badge as x-badge"
             "Find user user835611 on StackOverflow. You can accept all cookies. What reputation score do they have?. "
             "Return a summary of topics they contribute to. "
             "Has user835611 earned x-badge? "
         ),
-        llm=ChatOpenAI(model="gpt-4o"),
-        # browser=browser,
+        llm=llm,
     )
     result = await agent.run()
     print(result)
